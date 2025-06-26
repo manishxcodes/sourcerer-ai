94338fefddf9a835929204d8415b5100371fd842
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-06-27 02:56:33 +0530

    chore: clean the response of fetched data

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -5,18 +5,31 @@ export async function POST(req: NextRequest) {
     const {searchInput, searchType} = await req.json();
     console.log("POST /api/search hit");
 
-
     if(!searchInput) {
         return NextResponse.json({message: "searchInput required"});
     }
 
-    const result = await axios.get(`${process.env.GOOGLE_SEARCH_API_ENDPOINT}?key=${process.env.GOOGLE_SEARCH_API_KEY}&cx=${process.env.GOOGLE_ENGINE_ID}&q=${searchInput}`);
+    try {
+        const result = await axios.get(`${process.env.GOOGLE_SEARCH_API_ENDPOINT}?key=${process.env.GOOGLE_SEARCH_API_KEY}&cx=${process.env.GOOGLE_ENGINE_ID}&q=${searchInput}`);
 
-    console.log(result.data)
+        console.log(result.data)
 
-    return NextResponse.json({data: result.data});
-}
+        const items = result.data.items || [];
+        
+        const cleanedResponse = items.map((item: any) => (
+            {
+                title: item.title,
+                snippet: item.snippet,
+                link: item.link,
+                displayLink: item.displayLink,
+                ogImage: item.pagemap?.metatags?.[0]?.["og:image"],
+                cseImage: item.pagemap?.cse_image?.[0]?.src
+            }
+        ));
 
-export async function GET() {
-    return NextResponse.json({"hello": "there"});
+        return NextResponse.json({data: cleanedResponse});
+    } catch(err) {
+        console.log("error whle fetching answer");
+        return NextResponse.json({message: "Something went wrong while fetching answer", details: err});
+    }
 }
