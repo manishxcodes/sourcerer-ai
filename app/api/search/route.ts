44c9b0bbc43a88c6ee632610ff4905b0cad0a010
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-06-30 21:57:02 +0530

    add route to fetch news

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -75,3 +75,32 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({message: "Something went wrong while fetching answer", details: err});
     }
 }
+
+
+export async function GET() {
+    const searchInput = 'latest news on sport, tech and politics in india'
+
+    try {
+        const result = await axios.get(`${process.env.GOOGLE_SEARCH_API_ENDPOINT}?key=${process.env.GOOGLE_SEARCH_API_KEY}&cx=${process.env.GOOGLE_ENGINE_ID}&q=${searchInput}`);
+
+        const items = result.data.items || [];
+
+        const cleanedResponse = items.map((item: any) => (
+            {
+                title: item.title,
+                snippet: item.snippet,
+                link: item.link,
+                displayLink: item.displayLink,
+                ogImage: item.pagemap?.metatags?.[0]?.["og:image"],
+                cseImage: item.pagemap?.cse_image?.[0]?.src
+            }
+        ));
+
+        return NextResponse.json(cleanedResponse);
+    } catch(err) {
+        console.log('Error while fetching news', {details: err});
+        return NextResponse.json({message: "error whilre fetching news", details: err});
+
+    }
+
+}
