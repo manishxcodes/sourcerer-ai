9e00749a43536e02c6d9b6acdcb788aa9a82d55d
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-07-04 11:36:40 +0530

    style: remove ai model option

--- app/_components/chat-box.tsx
+++ app/_components/chat-box.tsx
@@ -2,16 +2,7 @@
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Button } from "@/components/ui/button"
-import { Atom, Cpu, SearchCheck, SendHorizonal } from "lucide-react"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu"
-import { AIModelsOption } from "../services/shared"
+import { Atom,  Loader, SearchCheck, SendHorizonal } from "lucide-react"
 import { useState } from "react"
 import { supabase } from "../services/supabase"
 import { useUserContext } from "../context/userContext"
@@ -80,25 +71,8 @@ export function ChatBox() {
                         </TabsList>
                     </div>
                     <div className="flex justify-center gap-x-2">
-                        <DropdownMenu>
-                            <DropdownMenuTrigger className="hover:bg-accent-foreground hover:text-primary hover:scale-105 transtion-all duration-100 pr-2" >
-                                <Cpu height={20} width={20} className="text-neutral-500"/>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent>
-                                <DropdownMenuLabel className="px-4">Choose Model</DropdownMenuLabel>
-                                <DropdownMenuSeparator />
-                                {
-                                    AIModelsOption.map((model) => (
-                                        <DropdownMenuItem key={model.id} className="w-full  hover:bg-primary/10 rounded-xs cursor-pointer px-4 py-2">
-                                            <h2 className="text-sm font-semibold">{model.name}</h2>
-                                            <p className="text-xs">{model.description}</p>
-                                        </DropdownMenuItem>
-                                    ))
-                                }
-                            </DropdownMenuContent>
-                        </DropdownMenu>
                         <Button onClick={onSearch} disabled={loading}>
-                            <SendHorizonal/>
+                            {loading ? <Loader className="animate-spin" /> :<SendHorizonal/>}
                         </Button>
                     </div>
                 </div>
