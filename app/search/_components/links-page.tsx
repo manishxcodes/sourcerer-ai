f7b73ade7cab52e1fe5da1eb0fa219450d7bc9cf
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-07-05 20:37:38 +0530

    style: give size to each link card

--- app/search/_components/links-page.tsx
+++ app/search/_components/links-page.tsx
@@ -29,7 +29,7 @@ export function LinksPage() {
                 searchResults && searchResults.searchResults.map((result, index) => {
                     const imageUrl = result.cseImage || result.ogImage || '';
                     return (
-                        <div key={index} className="px-2 py-2 rounded-md flex  group hover:scale-102 transition-all cursor-pointer shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+                        <div key={index} className="px-2 py-2 rounded-md flex  group hover:scale-102 transition-all cursor-pointer shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] h-24">
                             <a className="flex gap-x-4 " href={result.link} target="blank">
                                 <div className="flex-1/12  items-center w-full h-full ">
                                     {
